19fac5da06adcadf8931f05c9ff4ab5410b7a6a1
Intuitive Harmony <j.horst77@gmail.com>
2023-11-19 11:09:17 -0700

    encoder working

--- midiMessageMachine.ino
+++ midiMessageMachine.ino
@@ -8,12 +8,21 @@
 #define I2C_ADDRESS 0x3C         // display address 0X3C+SA0 - 0x3C or 0x3D
 #define RST_PIN -1               // Define proper display RST_PIN if required.
 
+
+#define encoderPinA 5  // Encoder Pins
+#define encoderPinB 6
+
+
 SSD1306AsciiAvrI2c oled;  // Instance of the display
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 
-uint8_t messageCount = 1;
+uint8_t messageCount = 1;  // Keep track of how many messages are on the screen
+int startRow = 0;          // Variable to control the starting row for displaying messages
+int aState;
+int aLastState = 0;  // Keep track of any encoder movements
+
 
 
 
@@ -27,6 +36,14 @@ void setup() {
 #endif  // RST_PIN >= 0
   // Call oled.setI2cClock(frequency) to change from the default frequency.
 
+
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+
+  pinMode(encoderPinA, INPUT_PULLUP);
+  pinMode(encoderPinB, INPUT_PULLUP);
+
+  aLastState = digitalRead(encoderPinA); // Initial read of the encoder state
+
   oled.setFont(Adafruit5x7);  // Set font for display
 
   oled.set2X();
@@ -36,18 +53,28 @@ void setup() {
 
   delay(3000);
   oled.clear();
-
-  MIDI.begin(MIDI_CHANNEL_OMNI);
 }
 
 void loop() {
+
+  Serial.print("startRow: ");
+  Serial.println(startRow);
+
+  aState = digitalRead(encoderPinA);
+  if(aState != aLastState) {
+    if (digitalRead(encoderPinB) != aState) {
+      startRow++;
+    } else {
+      startRow--;
+    }
+    aLastState = aState;
+  }
+
   // Check if there is any MIDI input
   if (MIDI.read()) {
     // Print the received MIDI message to the OLED display
-    // Print MIDI message
     oled.print(messageCount);
     oled.print(": ");
-
     // Use bitwise OR to get the full status byte
     byte fullStatusByte = MIDI.getType() | MIDI.getChannel();
     oled.print(fullStatusByte, HEX);
